38af22bf871c34240ac72bb693dc588bc1b2c016
Timothé <166731667+Timmattack@users.noreply.github.com>
2025-03-14 00:54:51 +0100

    ça marche O_o

--- sources/Analyse_ICS.py
+++ sources/Analyse_ICS.py
@@ -1,147 +1,117 @@
-from ics import Calendar, Event
+from ics import Calendar
+from ics.timeline import Timeline
 import arrow
-from datetime import datetime, date, timedelta
-from pytz import timezone
-from zoneinfo import ZoneInfo
 from os import walk
 import json
 
+from API import est_bat_A, est_bat_G, est_bat_I, est_bat_H, est_bat_L, make_name_simple
+
 """
 Objectif:
-    Récolter les moments où les salles sont libres (pas d'évènement à X moment)
+    Récolter les moments où lesest_bat_L salles sont libres (pas d'évènement à X moment)
     
-on se limitera à vérifier les horaires entre 7h30 et 19h00 (pour une journée)
+on se limitera à vérifier les horaires sur une journée
 
 1- charger le .ics
-2- récupérer les dates des évènements (la paire début et fin)
-3- ?? créer un ics de salles libre ??
+2- vérifier si la salle n'a pas d'évènement entre maintenant + 5min et maintenant + 20min
+3- créer un json des salles libres
 """
 
-''' tzdata names:
-TIMEZONE LIST EXEMPLES
-UTC +1: "Europe/Paris"
-UTC +0: "Zulu"
-UTC -5: "America/New_York"
-'''
-
-def debut_fin_event(event):
-    if(event):
-        return event.begin, event.end
-    else:
-        return None
-
-
-def Get_first_event(file: str):
-    with open(file, 'r', encoding='utf-8') as f:
-        ics_content = f.read()
+def filesWithFreeRoom(dossier, start, end):
+    filenames = next(walk(dossier), (None, None, []))[2]
     
-    calendar = Calendar(ics_content)
+    filenames = [dossier+file for file in filenames]
     
-    first_event = next(iter(calendar.events), None)
+    salles_libres = []
     
-    return first_event
-    
-
-def cool_print_date(date: arrow.arrow.Arrow) -> None:
-    print(date.year, "/", date.month, "/", date.day, "j", 
-    date.hour, ":", date.minute)
-
-
-def print_evenement_today(calendar: Calendar) -> None:
-
-    for element in calendar.events:
-
-        elementDate = element.begin.date()
-
-        if elementDate == date.today():
-            print("TODAY : ", element.begin, " WITH ", element.name)
-
-    
-def afficher_evenement_heure_locale(event: Event, tz: timezone) -> None:
-    begin = event.begin.astimezone(tz)
-    end = event.end.astimezone(tz)
-    print("- - -")
-    print("[DEBUT]>", begin, " [FIN]>", end, " [NOM]>", event.name)
-
-
-def temps_entre_deux_event( begin: datetime, time_check: datetime, end: datetime) -> bool:
-    if begin < time_check and time_check < end:
-        return True
-    
-    return False
-
-
-def est_salle_libre(salle_file_path: str, time_check: datetime) -> bool:
-
-    # on ouvre le fichier ics de la salle :
-    with open(salle_file_path, 'r') as file:
-        calendar = Calendar(file.read())
-
-    for element in calendar.events: # on regarde tout les evenements d'une salle, si aucun d'eux ne pose problème, alors elle est libre
-        if temps_entre_deux_event(element.begin ,time_check, element.end):
-            #afficher_evenement_heure_locale(element, timezone("Europe/Paris"))
-            return False
-        
-    return True
-
-
-def iterations_event(file_path: str) -> None:
-    # Open the ics file 
-    with open(file_path, 'r', encoding='utf-8') as file:
-        calendar = Calendar(file.read())
-
-    for element in calendar.walk():
-        if element.name == "VEVENT":
-            date_start = element.get('dtstart').dt
-            if isinstance(date_start, datetime):
-                date = date_start.date()
-            else:
-                date = date_start
-            if date == datetime.now():
-                print(date, " / ",element.get("summary"))
-
-
-
-def print_salles_libre_at():
-
-    chemin_salles = "../salles_edt/"
-    paris_tz = timezone("Europe/Paris")
-    customTime = datetime.now()
-    customTime = customTime.replace(tzinfo=paris_tz)
-
-    filenames = next(walk(chemin_salles), (None, None, []))[2]
-
-    filenames = [chemin_salles+file for file in filenames]
-
-    exportJson = {}
-
     for file in filenames:
-        #print(file)
-        if est_salle_libre(file, customTime):
-            #print(file, "est libre")
-            #print(file, " LIEN SALLE", )
-            
+        with open(file, "r", encoding="utf-8") as fichier:
+            contenu = fichier.read()
+    
+        cal = Calendar(contenu)
+        
+        timeline = Timeline(cal)
+        
+        overlap = list(timeline.overlapping(start, end))
+        
+        """
+        for event in overlap:
+            print(event.description)
+        print("---------------")
+        """
+        
+        if(not overlap):
+            salles_libres.append(file)
+    
+    
+    return salles_libres
 
-            
-            exportJson[file] = "est libre"
-        else:
-            print(file, "est pas libre")
 
-    print(exportJson)
-    print(len(exportJson))
+def extraitNomSalle(fic: str, dossier: str):
+    return fic[len(dossier):len(fic)-len(".ics")]
 
-    with open("../salles_libres/salles_libres_beta.json", "w", encoding="utf-8") as f:
-        json.dump(exportJson, f, ensure_ascii=False);
+
+def extraitID(fic: str):
+    with open(fic, "r", encoding="utf-8") as fichier:
+        contenu = fichier.read()
+
+    cal = Calendar(contenu)
+
+    premier_event = next(iter(cal.events), None)
+
+    return premier_event.uid[1:33]
+
+
+def InitDicSalle(nom: str, lien: str):
+    TheDict = {
+    "nom": "",
+    "lien": ""
+    }
+    
+    TheDict["nom"] = nom
+    TheDict["link"] = lien
+    
+    return TheDict
+
+
+def sallesBat_X(est_bat_X, fic_salles_libres: str, nom_salles_libres, RACINE: str = "https://edt.univ-angers.fr/edt/ressource?type=s9FDC055BB1C34F92E0530100007F467B&id="):  
+    
+    sallesBX = []
+    for fic, nom_salle in zip(fic_salles_libres, nom_salles_libres):
+        if est_bat_X(make_name_simple(nom_salle)):
+            id = extraitID(fic)
+            sallesBX.append(InitDicSalle(make_name_simple(nom_salle), RACINE+id))
+    
+    return sallesBX
 
 
 
 def main() -> None:
-
-
-
-    print_salles_libre_at()
-    #print(datetime.now())
-
+    dossier = "../salles_edt/"
+    
+    start = arrow.utcnow().replace(minute=+5)
+    end = arrow.utcnow().replace(minute=+20)
+    
+    fic_salles_libres = filesWithFreeRoom(dossier, start, end)
+    nom_salles_libres = [extraitNomSalle(fic, dossier) for fic in fic_salles_libres]
+    
+    JSON_Result = {
+    "Batiment A": [],
+    "Batiment G": [],
+    "Batiment H": [],
+    "Batiment I": [],
+    "Batiment L": []
+    }
+    
+    JSON_Result["Batiment A"] = sallesBat_X(est_bat_A, fic_salles_libres, nom_salles_libres)
+    JSON_Result["Batiment G"] = sallesBat_X(est_bat_G, fic_salles_libres, nom_salles_libres)
+    JSON_Result["Batiment H"] = sallesBat_X(est_bat_H, fic_salles_libres, nom_salles_libres)
+    JSON_Result["Batiment I"] = sallesBat_X(est_bat_I, fic_salles_libres, nom_salles_libres)
+    JSON_Result["Batiment L"] = sallesBat_X(est_bat_L, fic_salles_libres, nom_salles_libres)
+    
+    with open("../salles_libres/salles_libres.json", 'w') as f:
+        json.dump(JSON_Result, f)
+    
 
 if __name__ == "__main__":
     main()
